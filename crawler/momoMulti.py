5178d1f213780239bde39aba8550c7e2e790cfe2
UnCarter25le <UnCarter25le@gmail.com>
2019-07-02 13:54:02 +0800

    alter the error where fixed browser visit pages

--- crawler/momoMulti.py
+++ crawler/momoMulti.py
@@ -8,7 +8,6 @@
     
     2. python必須啟動瀏覽器才能『看到』momo的產品頁。
 
-    
 
 備　　註：
 
@@ -16,11 +15,14 @@
     "pics"、"picb"（設為None）、"name"、originprice"、"Id"、"produrl"
 
 
-#一個chromedriver就對付一頁，若是同一個chomedriver開啟網頁兩次，就會被擋。
 
 #2018-3-15   如果在執行splinter時，出現以下錯誤，下載最新的chromedriver，可以解決
 Message: chrome not reachable (Session info: chrome=65.0.3325.146) 
 (Driver info: chromedriver=2.33.506092 (733a02544d189eeb751fe0d7ddca79a0ee28cce4),platform=Linux 4.13.0-36-generic x86_64)
+
+
+30個process
+完成！一共耗時：925.360677242279 秒
 
 """
 
@@ -37,8 +39,9 @@ import sys
 import signal
 # from math import ceil
 import multiprocessing as mp
-from urllib3.exceptions import MaxRetryError
-from urllib3.exceptions import NewConnectionError
+# from urllib3.exceptions import MaxRetryError
+# from urllib3.exceptions import NewConnectionError
+from selenium.common.exceptions import TimeoutException
 
 _BASE_PATH = "/".join(os.path.abspath(__file__).split("/")[:-2])
 sys.path.append(_BASE_PATH)
@@ -46,6 +49,7 @@ sys.path.append(_BASE_PATH)
 from libs.manipulateDir import mkdirForRawData
 from libs.manipulateDir import eraseRawData
 from libs.time import timeSleepRandomly
+from libs.time import timeSleepEight
 from libs.time import timeSleepOne
 from libs.time import timeSleepTwo
 from libs.time import timeSleepFour
@@ -58,16 +62,19 @@ from libs.regex import interDiv
 from libs.splinterBrowser import buildSplinterBrowserHeadless
 from libs.splinterBrowser import browserWaitTime
 
+def requestsHandlingWhenTimeoutOccur(url, browserName):
+    timeSleepEight()
+    browser = buildSplinterBrowserHeadless(browserName)
+    timeSleepRandomly()
+    browser.visit(url)
         
 # 正式===============================================================================
 def getPageInARow(input, output, keywordUrlPair, objectiveFolder, objective):
     begin = timeCalculate()
     thisPID = os.getpid()
-    browser = buildSplinterBrowserHeadless('chrome')
     while True:
         print(thisPID,"===========================================")
         searchword = input.get()
-
         print('getPageInARow is in new process %s, %s ' % (getPageInARow_proc, os.getpid()))
         print()
         eraseRawData(objectiveFolder, objective, searchword)
@@ -75,17 +82,30 @@ def getPageInARow(input, output, keywordUrlPair, objectiveFolder, objective):
 
         url = keywordUrlPair[searchword]
 
-        timeSleepRandomly()
         try:
+            # 建立browser的代碼放進while True裡面，就可以避免「同一個瀏覽器」持續拜訪網頁時，被拒絕的情況。
+            timeSleepTwo()
+            browser = buildSplinterBrowserHeadless('chrome')
+            timeSleepRandomly()
             browser.visit(url)
-        except (ConnectionRefusedError, MaxRetryError, NewConnectionError) as e:
+        except (ConnectionRefusedError, TimeoutException) as e:
             print(f"{thisPID}__{getPageInARow_proc}  讀取{searchword}第 1 頁有問題。", e)
+            print(f"{thisPID}__{getPageInARow_proc}  重建browser物件，進行再處理!")
+            timeSleepFour()
+            browser = buildSplinterBrowserHeadless('chrome')
+            timeSleepRandomly()
+            browser.visit(url)
+            print(f"再處理{searchword}第 1 頁>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>成功！")
+        else:
+            print(f"讀取{searchword}第 1 頁>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>成功！")
 
         browserWaitTime(browser)
         
         timeSleepTwo()
         
         tempHtml = browser.html
+
+        timeSleepRandomly()
 
         soup = BeautifulSoup(tempHtml,'html.parser')
         
@@ -109,7 +129,9 @@ def getPageInARow(input, output, keywordUrlPair, objectiveFolder, objective):
             print(f"kill {thisPID}__{getPageInARow_proc} >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
             os.kill(thisPID, signal.SIGKILL)
         
-        timeSleepRandomly()
+        # 休息久一點，讓所有searchword的第一頁都有被讀到。
+        timeSleepEight()
+        timeSleepEight()
         
         for num in range(2, totalPage+1):
             strNum = str(num)
@@ -125,8 +147,8 @@ def getPageInARow(input, output, keywordUrlPair, objectiveFolder, objective):
 
 # 補救用===============================================================================
 # def getPageInARow(input, output, index, keywordUrlPair, dirName):
-#     browser = buildSplinterBrowserHeadless('chrome')
 #     while True:
+#         browser = buildSplinterBrowserHeadless('chrome')
 #         print('getPageInARow is in new process %s, %s ' % (getPageInARow_proc, os.getpid()))
 #         print()
         
@@ -146,27 +168,41 @@ def getPageInARow(input, output, keywordUrlPair, objectiveFolder, objective):
 def getPageInARowAdvanced(input, objectiveFolder, objective):
     begin = timeCalculate()
     thisPID = os.getpid()
-    browser = buildSplinterBrowserHeadless('chrome')
     while True:
+        print(thisPID,"===========================================")
         consecutiveUrl = input.get()
-
         searchword, page, totalPage, url = consecutiveUrl.split('+')
-
         print(url)
-        
         print(f"{thisPID}__{getPageInARowAdvanced_proc} 開始處理 {searchword} 的第 {page} 頁：")
         
-        timeSleepRandomly()
-        timeSleepFour()
-        
         try:
+            # 建立browser的代碼放進while True裡面，就可以避免「同一個瀏覽器」持續拜訪網頁時，被拒絕的情況。
+            timeSleepFour()
+            browser = buildSplinterBrowserHeadless('chrome')
+            timeSleepRandomly()
             browser.visit(url)
-        except ConnectionRefusedError as e:  
-            print(f"{thisPID}__{getPageInARowAdvanced_proc} 讀取 {searchword} 第 {page} 頁有問題。")
-            print(f"{thisPID}__{getPageInARowAdvanced_proc} 要break-----------------------------")
-            break
-        browserWaitTime(browser)
+        except (ConnectionRefusedError, TimeoutException) as e:
+            #要是頁面在此出錯，那麼要怎麼繼續處理錯誤的頁面？
+            print(f"{thisPID}__{getPageInARowAdvanced_proc} 讀取 {searchword} 第 {page} 頁有問題。",e)
+            print(f"{thisPID}__{getPageInARowAdvanced_proc} 重建browser物件，進行再處理-----------------------------")
+            # timeSleepFour()
+            # browser = buildSplinterBrowserHeadless('chrome')
+            # timeSleepRandomly()
+            # browser.visit(url)
+            try:
+                requestsHandlingWhenTimeoutOccur(url, "chrome")
+            except (ConnectionRefusedError, TimeoutException) as e:
+                print(f"{searchword}第 {page} 頁進行第三次處理。。。")
+                requestsHandlingWhenTimeoutOccur(url, "chrome")
+                print(f"再處理{searchword}第 {page} 頁，成功！")
+            else:
+                print(f"再處理{searchword}第 {page} 頁，成功！")
+        else:
+            print(f"讀取{searchword}第 {page} 頁，成功！")
+
         
+        browserWaitTime(browser)
+
         timeSleepTwo()
 
         tempHtml = browser.html
@@ -178,7 +214,7 @@ def getPageInARowAdvanced(input, objectiveFolder, objective):
         with open(f"{_BASE_PATH}/dataMunging/{objectiveFolder}/{objective}/{searchword}/{page}_{totalPage}_{searchword}.txt", 'w', encoding='utf-8')as f:
             f.write(str(soup))
         print()
-        print(f'{thisPID}  成功寫出  {searchword}  第{page}頁')
+        print(f'{thisPID}  成功寫出  {searchword}  第{page}頁，總共{totalPage} 頁。')
     
         try:
             browser.quit()
@@ -211,7 +247,7 @@ if __name__ == '__main__':
 
     # 啟動進程
     Process_1 = []
-    for p in range(1):
+    for p in range(8):
         getPageInARow_proc = mp.Process(target=getPageInARow, args=(searchword_queue, url_queue, _momoKeywordUrlPair, objectiveFolder, objective,))
         getPageInARow_proc.daemon = True
         getPageInARow_proc.start()
@@ -219,7 +255,7 @@ if __name__ == '__main__':
         Process_1.append(getPageInARow_proc)
     
     Process_2 = []
-    for u in range(60):
+    for u in range(30):
         getPageInARowAdvanced_proc = mp.Process(target=getPageInARowAdvanced, args=(url_queue,objectiveFolder, objective,))
         getPageInARowAdvanced_proc.daemon = True
         getPageInARowAdvanced_proc.start()
