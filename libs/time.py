b946f7efbe1e2ee62d1bee9cf1d057035deee933
UnCarter25le <UnCarter25le@gmail.com>
2019-08-02 12:19:57 +0800

    enrich the alternatives of duration

--- libs/time.py
+++ libs/time.py
@@ -35,7 +35,10 @@ def timeSleepOne():
 
 
 def timeSleepRandomly():
-    time.sleep(random.choice([2.6, 2.9, 1.8 ,2.8, 3.1, 1.5, 3.4, 4.1, 3.6]))
+    time.sleep(random.choice([1.5, 1.6, 1.7, 1.8, 1.9, 
+                            2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 
+                            3.0, 3.1, 3.2, 3.3 ,3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 
+                            4.0, 4.1, 4.2, 4.3, 4.4, 4.5]))
 
 
 def timeStampGenerator():
