8119c617edd92dcd6646710e99baed2dc8f90a63
UnCarter25le <UnCarter25le@gmail.com>
2019-08-21 16:04:09 +0800

    improve tryExcept and create badRequest folder

--- crawler/pchomeApiMulti.py
+++ crawler/pchomeApiMulti.py
@@ -11,6 +11,7 @@
     
     多進程，估計共耗時：998.5998482704163 秒  5/29/2019
     getPageInARow 累計耗時：2170.309236764908 秒  6/22/2019
+    getPageInARow 累計耗時：3222.1281599998474 秒 8/5/2019
 
 備　　註：
 
@@ -18,6 +19,9 @@
     "pics"、"picb"、"name"、originprice"、"Id"、"produrl"
 
     以關鍵字「冷暖空調」在pchomeAPI的三個分類做查詢的品項是4388，還未把非真正冷氣空調的去掉。
+
+
+    try except 要改善  已有改善，待嘗試。
 
 """
 
@@ -41,37 +45,32 @@ from libs.manipulateDir import mkdirForRawData
 from libs.manipulateDir import eraseRawData
 from libs.multiProcessing import distributeKeyword
 from libs.multiProcessing import _pchomeKeywordUrlPair
-from libs.time import timeSleepEight
-from libs.time import timeSleepRandomly
-from libs.time import timeSleepOne
+from libs.timeWidget import timeSleepEight
+from libs.timeWidget import timeSleepRandomly
+from libs.timeWidget import timeSleepOne
 from libs.regex import randomChoice
 
 def getPageFirst(searchword, keyword, headers):
     url = "https://ecshweb.pchome.com.tw/search/v3.3/{0}/results?q={1}&page=1&sort=sale/dc".format(keyword, searchword)
-    res = requests.get(url, headers=headers)
-    timeSleepRandomly()
-    res.encoding = 'utf-8'
-    try:
-        jsonPage = json.loads(res.text)
-        totalPage = jsonPage['totalPage']
-        totalRows = jsonPage['totalRows']
-        timeSleepEight()
-        timeSleepRandomly()
-        return totalPage, totalRows
-    except JSONDecodeError as e:  #拜訪太密集的話，pchome回傳的json檔案格式就不會是正常的格式，因此會發生無法json反序列化的例外。
-        print(f"getPageFirst {keyword}  {searchword} 這裡發生錯誤   "+str(e)+"正在處理中。")
-        timeSleepEight()
-        timeSleepRandomly()
-        
-        res = requests.get(url, headers=headers)
-        res.encoding = 'utf-8'
-        timeSleepRandomly()
-
-        jsonPage = json.loads(res.text)
-        totalPage = jsonPage['totalPage']
-        totalRows = jsonPage['totalRows']
-        timeSleepEight()
-        return totalPage, totalRows
+    for i in range(3):
+        try:
+            res = requests.get(url, headers=headers)
+            timeSleepRandomly()
+            timeSleepOne()
+            res.encoding = 'utf-8'
+            jsonPage = json.loads(res.text)
+            totalPage = jsonPage['totalPage']
+            totalRows = jsonPage['totalRows']
+            timeSleepEight()
+            timeSleepRandomly()
+            break
+        except JSONDecodeError as e:  #拜訪太密集的話，pchome回傳的json檔案格式就不會是正常的格式，因此會發生無法json反序列化的例外。
+            print(f"getPageFirst {keyword}  {searchword} 這裡發生錯誤   "+str(e)+"正在處理中。")
+            timeSleepEight()
+            timeSleepRandomly()
+            totalPage = "99999"
+            totalRows = "99999"
+    return totalPage, totalRows
 
 # 進程worker不能有關鍵字引數、以及**kwargs
 def getPageInARow(input, headers, objectiveFolder, objective, *args):
@@ -87,39 +86,42 @@ def getPageInARow(input, headers, objectiveFolder, objective, *args):
         mkdirForRawData(objectiveFolder, objective, searchword, keyword=keyword)
 
         totalPage, totalRows = getPageFirst(searchword, keyword, headers)
+        try:
+            totalPagePlusOne = totalPage+1
+        except TypeError as e:
+            print("getPageFirst 出錯", e)
+            raise
+
 
         print(f"關鍵字 {searchword} 從{keyword}上取得資料，一共有 {totalPage} 頁， {totalRows}筆。")
-        # countRaws += int(totalRows)
-        # countPages += int(totalPage)
 
-        for page in range(1, totalPage+1):
+        for page in range(1, totalPagePlusOne):
             url = 'https://ecshweb.pchome.com.tw/search/v3.3/{0}/results?q={1}&page={2}&sort=sale/dc'.format(keyword, searchword, page)
-            res = requests.get(url, headers=headers)
-            res.encoding = 'utf-8'
+            
+            for i in range(3):
+                try:
+                    res = requests.get(url, headers=headers)
+                    res.encoding = 'utf-8'
+                    timeSleepRandomly()
+                    timeSleepOne()
+                    jsonPage = json.loads(res.text)
+                    timeSleepEight()
+                    timeSleepRandomly()
+                    break
+                except JSONDecodeError as e:
+                    print(f"getPageInARow這裡發生錯誤  {keyword}_{searchword}_{page} "+str(e)+"正在處理中。")
+                    timeSleepEight()
+                    timeSleepRandomly()
+                    jsonPage = ""
 
-            timeSleepRandomly()
-
-            try:
-                jsonPage = json.loads(res.text)
-                with open(f"{_BASE_PATH}/dataMunging/{objectiveFolder}/{objective}/{searchword}/{keyword}/{page}_{totalPage}_{totalRows}_{keyword+searchword}.json", 'w', encoding='utf-8')as f:
-                    json.dump(jsonPage, f, indent=2, ensure_ascii=False)
-                print("成功寫出  {0}  第 {1} 頁，共 {2} 頁".format(keyword+searchword, page, totalPage))
-                timeSleepEight()
-                timeSleepRandomly()
-            except JSONDecodeError as e:
-                print(f"getPageInARow這裡發生錯誤  {keyword}_{searchword}_{page} "+str(e)+"正在處理中。")
-                timeSleepEight()
-                timeSleepRandomly()
-                
-                res = requests.get(url, headers=headers)
-                res.encoding = 'utf-8'
-                timeSleepRandomly()
-
-                jsonPage = json.loads(res.text)
-                with open(f"{_BASE_PATH}/dataMunging/{objectiveFolder}/{objective}/{searchword}/{keyword}/{page}_{totalPage}_{totalRows}_{keyword+searchword}.json", 'w', encoding='utf-8')as f:
-                    json.dump(jsonPage, f, indent=2, ensure_ascii=False)
-                print("成功寫出  {0}  第 {1} 頁，共 {2} 頁".format(keyword+searchword, page, totalPage))
-                timeSleepEight()
+            if not jsonPage:
+                badRequestRoute = f"{_BASE_PATH}/dataMunging/{objectiveFolder}/{objective}/badRequest"
+                with open(f"{badRequestRoute}/badRequest_{searchword}.txt", "a",  newline='', encoding='utf-8')as f: # newline沒作用...
+                    errorMessage = url + "\n"
+                    f.write(errorMessage)   #writelines作用在errorMessage是list時
+            with open(f"{_BASE_PATH}/dataMunging/{objectiveFolder}/{objective}/{searchword}/{keyword}/{page}_{totalPage}_{totalRows}_{keyword+searchword}.json", 'w', encoding='utf-8')as f:
+                json.dump(jsonPage, f, indent=2, ensure_ascii=False)
+            print("成功寫出  {0}  第 {1} 頁，共 {2} 頁".format(keyword+searchword, page, totalPage))
 
         print(f"這裡是getPageInARow_{thisPID}，準備完成{keyword}_{searchword}工作。 ")
         print()
@@ -140,12 +142,16 @@ if __name__ == '__main__':
     begin = time.time()
     print('start in main process %s' %os.getpid())
 
+    eraseRawData(objectiveFolder, objective, "badRequest")
+    mkdirForRawData(objectiveFolder, objective, "badRequest")
+    print('-------------------------------------------------------------------------')
+
     # 共同佇列宣告
     keyword_queue = mp.JoinableQueue()
 
     # 啟動行程
     Process_1 = []
-    for p in range(5): #開5個進程時，沒有出錯。
+    for p in range(4): #開5個進程時，沒有出錯。
         getPageInARow_proc = mp.Process(target=getPageInARow, args=(keyword_queue, headers, objectiveFolder, objective,))#*args
         getPageInARow_proc.daemon = True
         getPageInARow_proc.start()
@@ -155,7 +161,6 @@ if __name__ == '__main__':
 
     # 主行程    
     keywordList = ["24h", "vdr", "kdn"]# 24小時、購物中心、代購服務
-
     searchwordAndKeyword = [rowOutside + "+" + rowInside for rowOutside in _pchomeKeywordUrlPair for rowInside in keywordList]
     distributeKeyword(searchwordAndKeyword, keyword_queue)
 
@@ -171,6 +176,6 @@ if __name__ == '__main__':
         print(f'{proc} has terminated!')
 
     end = time.time()
-    # print(f"關鍵字 {searchword} 從24h、購物中心、代購服務上取得資料，一共有 {countPages} 頁， {countRaws}筆。")
+    
     print("完成！一共耗時：{0} 秒".format(end-begin)) 
 
