d0e05a485161f435d92c915d32d0f9cc6683b849
UnCarter25le <UnCarter25le@gmail.com>
2019-08-29 17:35:24 +0800

    complete news titles insert

--- database/insertLatestDataIntoDB.py
+++ database/insertLatestDataIntoDB.py
@@ -7,6 +7,8 @@
 
 備　　註：
 
+    新檔案 bureauEnergy 花費： 242.81847214698792 秒。
+
     能源局 、pchome、momo 花費： 3356.2910916805267 秒
 
 """
@@ -31,6 +33,7 @@ from libs.sqlDMLAndsqlAlchemyORM import (
                                         ecommerceMunging,
                                         ecommerceMungingHistorical,
                                         weatherMunging,
+                                        newsTitleFromSeleniumMunging,
                                         multiSourceObjectInitial
                                         )
 from libs.timeWidget import timeCalculate
@@ -46,16 +49,19 @@ if __name__ == '__main__':
     tableClassBase = sqlObjectInitail()._tableClassBase
     engine = tableClassBase.connectToMySQLEngine()
     
-    multiSourceObject = multiSourceObjectInitial(bureauEnergyM=bureauEnergyMunging(), ecommerceM=ecommerceMunging(), weatherM=weatherMunging())
-    multiSourceObjectForBackupTable = multiSourceObjectInitial(bureauEnergyM=bureauEnergyMungingHistorical(), ecommerceM=ecommerceMungingHistorical())
+    multiSourceObject = multiSourceObjectInitial(bureauEnergyM=bureauEnergyMunging(), 
+                                                ecommerceM=ecommerceMunging(), 
+                                                weatherM=weatherMunging(), 
+                                                newsM=newsTitleFromSeleniumMunging())
+
+    multiSourceObjectForBackupTable = multiSourceObjectInitial(bureauEnergyM=bureauEnergyMungingHistorical(), 
+                                                            ecommerceM=ecommerceMungingHistorical())
     
     bureauEnergyFolder = multiSourceObject.bureauEnergyM._objectiveBureauEnergy
     pchomeFoler = multiSourceObject.ecommerceM._objectivePchome
     momoFolder = multiSourceObject.ecommerceM._objectiveMomo
     weatherFolder = multiSourceObject.weatherM._objectiveWeather
-    
-    
-    
+    newsIntegraFolder = multiSourceObject.newsM._objectiveNewsIntegra
     
 
     
@@ -73,10 +79,11 @@ if __name__ == '__main__':
 
     """
     PART (2)-1
-    寫入 能源局產品-------------------------------------
+    寫入 能源局產品 到 bureau_energy_products 表格-------------------------------------
     This session is one-time procedure!
     """
     # begin = timeCalculate()
+    # multiSourceObject.bureauEnergyM.truncateTable(1, sourceDataFolderName=bureauEnergyFolder)
     # multiSourceObject.writeLatestDataIntoDB(multiSourceObject.bureauEnergyM, 
     #                 sourceDataFolderName=bureauEnergyFolder, bureauSET=bureauSET())
     # end = timeCalculate()
@@ -86,8 +93,8 @@ if __name__ == '__main__':
     
     """
     PART (2)-2
-    將最新的檔案寫入backup表格
-    This session is alternative;Please refer to 'inserHistoricalDataIntoDB.py".
+    將最新的 能源局產品 檔案寫入bureau_energy_products_backup 表格
+    This session is alternative;Please refer to 'inserHistoricalDataIntoDB.py  PART (1)-2".
     """
     # begin = timeCalculate()
     # multiSourceObjectForBackupTable.bureauEnergyM.alterStillWorkToZero(1)
@@ -102,10 +109,11 @@ if __name__ == '__main__':
 
     """
     PART (3)-1
-    寫入 電商商品-------------------------------------
+    寫入 電商商品 到 ecommerce_products 表格-------------------------------------
     This session is one-time procedure!
     """
     # begin = timeCalculate()
+    # # multiSourceObject.ecommerceM.truncateTable(1, sourceDataFolderName=pchomeFoler)
     # multiSourceObject.writeLatestDataIntoDB(multiSourceObject.ecommerceM,
     #                 sourceDataFolderName=pchomeFoler)
     # end = timeCalculate()
@@ -122,8 +130,8 @@ if __name__ == '__main__':
 
     """
     PART (3)-2
-    將最新的檔案寫入backup表格
-    This session is alternative;Please refer to 'inserHistoricalDataIntoDB.py".
+    將最新的 電商商品 檔案寫入 ecommerce_products_backup 表格 ----------連同價格異動的情況送進 ecommerce_products_price_records資料表--------------------------
+    This session is alternative;Please refer to 'inserHistoricalDataIntoDB.py PART (2)-2".
     """
 
     # begin = timeCalculate()
@@ -143,7 +151,7 @@ if __name__ == '__main__':
 
     """
     PART (4)
-    將氣象歷史資料入DB-------------------------------------
+    將氣象歷史資料 寫入  weather_records_by_months 表格-------------------------------------
     This session is one-time procedure!
     """
     # begin = timeCalculate()
@@ -153,6 +161,35 @@ if __name__ == '__main__':
     #                                 sourceDataFolderName=weatherFolder)
     # end = timeCalculate()
     # print(f"最新檔案 {weatherFolder} 花費：", end-begin, "秒。")
+
+
+
+
+    """
+    PART (5)-1
+    將 新聞標題 歷史資料 寫入 news_title_from_selenium 表格-------------------------------------
+    This session is one-time procedure!
+    """
+    # begin = timeCalculate()
+
+    # multiSourceObject.newsM.truncateTable(1, sourceDataFolderName=newsIntegraFolder)
+    # multiSourceObject.writeLatestDataIntoDB(multiSourceObject.newsM, 
+    #                                 sourceDataFolderName=newsIntegraFolder)
+    # end = timeCalculate()
+    # print(f"歷史檔案 {newsIntegraFolder} 花費：", end-begin, "秒。")
+
+
+    """
+    PART (5)-2
+    將 新聞標題 最新的資料 寫入 news_title_from_selenium 表格-------------------------------------
+    This session is one-time procedure!
+    """
+    # begin = timeCalculate()
+
+    # multiSourceObject.writeLatestDataIntoDB(multiSourceObject.newsM, 
+    #                                 sourceDataFolderName=newsIntegraFolder, latestBoolean=1)
+    # end = timeCalculate()
+    # print(f"最新檔案 {newsIntegraFolder} 花費：", end-begin, "秒。")
 
 
 
