780b5a27c293739e43e212910d91e2be9726040b
UnCarter25le <UnCarter25le@gmail.com>
2019-09-01 19:10:25 +0800

    complete structures

--- crawler/selectedNewsMulti.py
+++ crawler/selectedNewsMulti.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 """
-程式名稱：
+程式名稱：selectedNewsMulti.py
 程式描述：
 
 
@@ -9,6 +9,7 @@
 
    
 
+花費： 1282.5719978809357 秒
 """
 
 
@@ -22,103 +23,157 @@ from collections import Counter
 _BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(_BASE_PATH)
 
-from libs.mining import _newsUrlCheckDict
-from libs.regex import searchWordTrueOrFalse
-
-
-def judgeFolderFiles(dirRoute):
-   fileName = os.listdir(dirRoute)
-   if not fileName:
-      return fileName, 0
-   else:
-      return fileName, len(fileName)
-
-def loadOneFileIn(fileRoute):
-   with open(fileRoute)as f:
-      jsonFile = json.load(f)
-   return jsonFile
+from libs.mining import newsMining
+from libs.munging import rawDataMunging
+from libs.timeWidget import (
+                           timeStampGenerator,
+                           timeCalculate)
+from libs.sqlDMLAndsqlAlchemyORM import selectedNewsContentMunging
+from libs.manipulateDir import mkdirForCleanData
 
 
 if __name__ == '__main__':
 
-      objectiveFolder = "rawData"
+   newsMining = newsMining()
 
-      objectiveFolderClean = "cleanData"
+   mkdirForCleanData(rawDataMunging._objectiveFolderCleanData, rawDataMunging._objectiveFolderNewsWithContent)
 
-      objectiveFolderDataMining = "dataMining"
+   dirRoute = f"{rawDataMunging._dirRouteMungingClean}{rawDataMunging._objectiveFolderNews}/"
 
-      objectiveFolderDictionary = "dictionary"
 
-      objective = "news"
+   begin = timeCalculate()
 
-      dirRoute = f"{_BASE_PATH}/dataMunging/{objectiveFolderClean}/{objective}/"
+   fileName, fileNums = newsMining.judgeFolderFiles(dirRoute)
+
+   referenceFile = selectedNewsContentMunging().loadReferenceFileIn()
+
+   if fileNums == 0:
+      print("""
+            尚未有clean的newsUri可以爬！請先完成
+            googleNewsMulti.py --> 
+            newsMunging.py --> 
+            jiebaForNewsTitle.py --> 
+            copewithNewsUrlForCrawling.py
+            """)
+
+   elif fileNums == 1:
+      print("處理最新檔案(一個)：", fileName[0])
+      latestFileJson = newsMining.loadOneFileIn(dirRoute + fileName[0])
+      
+      readyLinkObject = latestFileJson["newsUrl"]
+
+
+      newsDictOutter = {}
+      newsDictInner = {}
+
+      for newsLink in readyLinkObject:
+         publisher_Id, publisher, videoLinkInContent, newsContent = newsMining.judgeHowToCrawl(newsLink, referenceFile)
+
+         if publisher and newsContent: #被捕捉到的新聞才做處理
+            newsTitle = readyLinkObject[newsLink][0]
+            publishDate = readyLinkObject[newsLink][2]   
+            # 組裝json
+            newsDictInner[newsLink] = {"newsTitle":newsTitle,
+                                       "publisher":publisher,
+                                       "publisher_Id":publisher_Id,
+                                       "publishDate":publishDate,
+                                       "videoLinkInContent":videoLinkInContent,
+                                       "newsContent":newsContent}
+            
+         elif publisher and not newsContent:# 爬取錯誤的處理
+            newsTitle = readyLinkObject[newsLink][0]
+            publishDate = readyLinkObject[newsLink][2]   
+            # 組裝json
+            newsDictInner[newsLink] = {"newsTitle":newsTitle,
+                                       "publisher":publisher,
+                                       "publisher_Id":publisher_Id,
+                                       "publishDate":publishDate,
+                                       "videoLinkInContent":videoLinkInContent,
+                                       "newsContent":newsContent}
+      
+      dataTime = latestFileJson["dateTime"] # 這裡的時間同標題篩選過後的檔案的dateTime
+      keyword = latestFileJson["keyword"]
+      newsTotalNum = len(newsDictInner)
+
+      newsDictOutter["dateTime"] = dataTime
+      newsDictOutter["keyword"] = keyword
+      newsDictOutter["newsTotalNum"] = newsTotalNum
+      newsDictOutter["newsUrl"] = newsDictInner
+
+      with open(f"{rawDataMunging._dirRouteMungingClean}{rawDataMunging._objectiveFolderNewsWithContent}/googleNews_{dataTime}_{newsTotalNum}_{keyword}.json", "w", encoding="utf-8")as f:
+         json.dump(newsDictOutter, f, indent=2, ensure_ascii=False)
+      
+      print(f"寫出檔案： googleNews_{dataTime}_{newsTotalNum}_{keyword}.json" )
+
+   else:
+
+      latestFileName =  fileName[-1]
+      forCheckFileName = fileName[-2]
+      latestFileJson = newsMining.loadOneFileIn(dirRoute + latestFileName)
+      forCheckFileJson = newsMining.loadOneFileIn(dirRoute + forCheckFileName)
+
+      latestFileSet = set(latestFileJson["newsUrl"])
+      forCheckFileSet = set(forCheckFileJson["newsUrl"])
 
       
-      fileName, fileNums = judgeFolderFiles(dirRoute)
+      print("最新檔案", latestFileName, "的新聞數：", len(latestFileSet))
+      print("上回檔案", forCheckFileName, "的新聞數：", len(forCheckFileSet))
+      # in-place差集
+      latestFileSet.difference_update(forCheckFileSet)
+      leftRemaining = len(latestFileSet)
 
-      if fileNums == 0:
-         print("尚未有clean的newsUri可以爬！請先完成googleNewsMulti.py --> newsMunging.py --> jiebaForNewsTitle.py --> copewithNewsUrlForCrawling.py")
-      elif fileNums == 1:
-         latestFileJson = loadOneFileIn(dirRoute + fileName[0])
+      if leftRemaining:
+         differenceDict = {url: latestFileJson["newsUrl"][url] for url in latestFileSet}
          
-         readyLinkObject = latestFileJson["newsUrl"]
+         print(len(differenceDict), latestFileSet)
 
-         for newsLink in readyLinkObject:
-            for checkLink in _newsUrlCheckDict:
-               if searchWordTrueOrFalse(checkLink, newsLink):
-                  publisher = _newsUrlCheckDict.get(checkLink)
-                  print(publisher)
-                  break
-               else:
-                  pass
-            
-            if publisher == "udn聯合新聞網":
-               pass
-            elif publisher == "經濟日報":
-               pass
-            elif publisher == "自由時報電子報":
-               pass
-            elif publisher == "中時電子報":
-               pass
-            elif publisher == "蘋果日報":
-               pass
-            elif publisher == "Yahoo奇摩新聞":
-               pass
-            elif publisher == "ETtoday":
-               pass
-            elif publisher == "NOWnews":
-               pass
-            elif publisher == "TVBS新聞":
-               pass
+         # for newsLink in differenceDict:
+         #    publisher_Id, publisher, newsContent = newsMining.judgeWhichLinkToCrawl(newsLink, referenceFile)
+
+         newsDictOutter = {}
+         newsDictInner = {}
+
+         for newsLink in differenceDict:
+            publisher_Id, publisher, videoLinkInContent, newsContent = newsMining.judgeHowToCrawl(newsLink, referenceFile)
+
+            if publisher and newsContent: #被捕捉到的新聞才做處理
+               newsTitle = differenceDict[newsLink][0]
+               publishDate = differenceDict[newsLink][2]   
+               # 組裝json
+               newsDictInner[newsLink] = {"newsTitle":newsTitle,
+                                       "publisher":publisher,
+                                       "publisher_Id":publisher_Id,
+                                       "publishDate":publishDate,
+                                       "videoLinkInContent":videoLinkInContent,
+                                       "newsContent":newsContent}
+            elif publisher and not newsContent:# 爬取錯誤的處理
+               newsTitle = differenceDict[newsLink][0]
+               publishDate = differenceDict[newsLink][2]   
+               # 組裝json
+               newsDictInner[newsLink] = {"newsTitle":newsTitle,
+                                       "publisher":publisher,
+                                       "publisher_Id":publisher_Id,
+                                       "publishDate":publishDate,
+                                       "videoLinkInContent":videoLinkInContent,
+                                       "newsContent":newsContent}
+         
+         dataTime = latestFileJson["dateTime"] # 這裡的時間同標題篩選過後的檔案的dateTime
+         keyword = latestFileJson["keyword"]
+         newsTotalNum = len(newsDictInner)
+
+         newsDictOutter["dateTime"] = dataTime
+         newsDictOutter["keyword"] = keyword
+         newsDictOutter["newsTotalNum"] = newsTotalNum
+         newsDictOutter["newsUrl"] = newsDictInner
+
+         with open(f"{rawDataMunging._dirRouteMungingClean}{rawDataMunging._objectiveFolderNewsWithContent}/googleNews_{dataTime}_{newsTotalNum}_{keyword}.json", "w", encoding="utf-8")as f:
+            json.dump(newsDictOutter, f, indent=2, ensure_ascii=False)
+
+         print(f"寫出檔案： googleNews_{dataTime}_{newsTotalNum}_{keyword}.json" )
 
       else:
-         #可按時間升冪排列
-         fileName.sort(key= lambda x: x.split('_')[1])
-
-         latestFile =  fileName[-1]
-         forCheckFile = fileName[-2]
-         latestFileJson = loadOneFileIn(dirRoute + latestFile)
-         forCheckFileJson = loadOneFileIn(dirRoute + forCheckFile)
-
-         latestFileSet = set(latestFileJson["newsUrl"])
-         forCheckFileSet = set(forCheckFileJson["newsUrl"])
-
-         
-         print("最新檔案", latestFile, "的新聞數：", len(latestFileSet))
-         print("上回檔案", forCheckFile, "的新聞數：", len(forCheckFileSet))
-         # in-place差集
-         latestFileSet.difference_update(forCheckFileSet)
-         leftRemaining = len(latestFileSet)
-
-         if leftRemaining:
-            differenceDict = {url: latestFileJson["newsUrl"][url] for url in latestFileSet}
-            
-            
-            print(len(differenceDict))
-            # for url in differenceDict:
-               
-
-         else:
-            print("無須爬蟲！")
+         print("無須爬蟲！")
     
 
+   end = timeCalculate()
+   print("花費：", end-begin, "秒。")
