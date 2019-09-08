a0925df55966830e624636aab1b3ce1f186713cc
UnCarter25le <UnCarter25le@gmail.com>
2019-09-08 16:50:18 +0800

    improve the main()

--- database/insertHistoricalDataIntoDB.py
+++ database/insertHistoricalDataIntoDB.py
@@ -53,6 +53,87 @@ from libs.manipulateDir import (
                                     initialFileFourthUnderscoreString
                                 )
 
+def writeBureauHistoricalData(multiSourceObject, sourceDataFolderName=None, bureauSET=None, alterStillWork=None):
+
+    """
+    PART (1)-1
+    歷史檔案 能源局產品 寫入bureau_energy_products_backup 表格-------------------------------------
+
+    This session is one-time procedure!   No need to do it again if done.
+    """
+
+    if not alterStillWork:
+        begin = timeCalculate()
+        multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.bureauEnergyM, 
+                            sourceDataFolderName=sourceDataFolderName, bureauSET=bureauSET)
+        end = timeCalculate()
+        print(f"歷史檔案 {sourceDataFolderName} 花費：", end-begin, "秒。")
+
+    else:
+
+        """
+        PART (1)-2
+        最新檔案 能源局產品  寫入bureau_energy_products_backup 表格-------------------------------------
+
+        This session is alternative;We are allowed to execute programs here or at "insertLatestDataIntoDB.py  PART (2)-2"。
+        What worth giving attention is whether historical data in DB at first?
+        """
+
+
+        begin = timeCalculate()
+        multiSourceObject.bureauEnergyM.alterStillWorkToZero(alterStillWork)
+        multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.bureauEnergyM, 
+                            sourceDataFolderName=sourceDataFolderName, bureauSET=bureauSET, latestBoolean=1)
+        end = timeCalculate()
+        print(f"最新檔案 {sourceDataFolderName} 花費：", end-begin, "秒。")
+
+
+
+def writeEcommerceHistoricalData(multiSourceObject, sourceDataFolderName_1=None, sourceDataFolderName_2=None, alterStillWork=None):
+    
+    
+    """
+    PART (2)-1
+    歷史檔案 電商產品 寫入 ecommerce_products_backup 表格 
+    ----------連同價格異動的情況送進 ecommerce_products_price_records資料表--------------------------
+    This session is one-time procedure!   No need to do it again if done.
+    """
+    if not alterStillWork:
+        begin = timeCalculate()
+        multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.ecommerceM, 
+                                        sourceDataFolderName=sourceDataFolderName_1)
+        end = timeCalculate()
+        print(f"歷史檔案 {sourceDataFolderName_1} 花費：", end-begin, "秒。")
+
+
+        begin = timeCalculate()
+        multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.ecommerceM, 
+                                        sourceDataFolderName=sourceDataFolderName_2)
+        end = timeCalculate()
+        print(f"歷史檔案 {sourceDataFolderName_2} 花費：", end-begin, "秒。")
+
+    else:
+        """
+        PART (2)-2
+        最新檔案 電商產品 寫入 ecommerce_products_backup 表格 
+        -----------連同價格異動的情況送進 ecommerce_products_price_records資料表--------------------------
+        This session is alternative;We are allowed to execute programs here or at "insertLatestDataIntoDB.py  PART (3)-2"。
+        What worth giving attention is whether historical data in DB at first?
+        """
+        begin = timeCalculate()
+
+        multiSourceObject.ecommerceM.alterStillWorkToZero(alterStillWork)
+        multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.ecommerceM, 
+                                        sourceDataFolderName=sourceDataFolderName_1, latestBoolean=1)
+        end = timeCalculate()
+        print(f"最新檔案 {sourceDataFolderName_1}, {sourceDataFolderName_2} 花費：", end-begin, "秒。")
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     tableClassBase = writeDataWhenMunging._tableClassBase
@@ -70,71 +151,34 @@ if __name__ == '__main__':
     #-------------------------------------
 
     """
-    PART (1)-1
-    歷史檔案 能源局產品 寫入bureau_energy_products_backup 表格-------------------------------------
-
-    This session is one-time procedure!   No need to do it again if done.
+    PART (1)-1 This session is one-time procedure!   No need to do it again if done.
     """
-    # begin = timeCalculate()
-    # multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.bureauEnergyM, 
-    #                     sourceDataFolderName=bureauEnergyFolder, bureauSET=bureauSET())
-    # end = timeCalculate()
-    # print(f"歷史檔案 {bureauEnergyFolder} 花費：", end-begin, "秒。")
-
+    writeBureauHistoricalData(multiSourceObject, 
+                        sourceDataFolderName=bureauEnergyFolder, bureauSET=bureauSET())
   
 
     """
-    PART (1)-2
-    最新檔案 能源局產品  寫入bureau_energy_products_backup 表格-------------------------------------
-
-    This session is alternative;We are allowed to execute programs here or at "insertLatestDataIntoDB.py  PART (2)-2"。
+    PART (1)-2 This session is alternative;
+    We are allowed to execute programs here or at "insertLatestDataIntoDB.py  PART (2)-2"。
     What worth giving attention is whether historical data in DB at first?
     """
-
-
-    # begin = timeCalculate()
-    # multiSourceObject.bureauEnergyM.alterStillWorkToZero(1)
-    # multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.bureauEnergyM, 
-    #                     sourceDataFolderName=bureauEnergyFolder, bureauSET=bureauSET(), latestBoolean=1)
-    # end = timeCalculate()
-    # print(f"最新檔案 {bureauEnergyFolder} 花費：", end-begin, "秒。")
-
-
+    writeBureauHistoricalData(multiSourceObject, 
+                        sourceDataFolderName=bureauEnergyFolder, bureauSET=bureauSET(), alterStillWork=1)
 
 
     """
-    PART (2)-1
-    歷史檔案 電商產品 寫入 ecommerce_products_backup 表格 
-    ----------連同價格異動的情況送進 ecommerce_products_price_records資料表--------------------------
-    This session is one-time procedure!   No need to do it again if done.
+    PART (2)-1 This session is one-time procedure!   No need to do it again if done.
     """
-    # begin = timeCalculate()
-    # multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.ecommerceM, 
-    #                                 sourceDataFolderName=pchomeFoler)
-    # end = timeCalculate()
-    # print(f"歷史檔案 {pchomeFoler} 花費：", end-begin, "秒。")
-
-
-    # begin = timeCalculate()
-    # multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.ecommerceM, 
-    #                                 sourceDataFolderName=momoFolder)
-    # end = timeCalculate()
-    # print(f"歷史檔案 {momoFolder} 花費：", end-begin, "秒。")
-
+    
+    writeEcommerceHistoricalData(multiSourceObject, 
+                        sourceDataFolderName_1=pchomeFoler, sourceDataFolderName_2=momoFolder)
 
     """
-    PART (2)-2
-    最新檔案 電商產品 寫入 ecommerce_products_backup 表格 
-    -----------連同價格異動的情況送進 ecommerce_products_price_records資料表--------------------------
-    This session is alternative;We are allowed to execute programs here or at "insertLatestDataIntoDB.py  PART (3)-2"。
+    PART (2)-2 This session is alternative;
+    We are allowed to execute programs here or at "insertLatestDataIntoDB.py  PART (3)-2"。
     What worth giving attention is whether historical data in DB at first?
     """
-    # begin = timeCalculate()
-
-    # multiSourceObject.ecommerceM.alterStillWorkToZero(1)
-    # multiSourceObject.writeHistoricalDataIntoDB(multiSourceObject.ecommerceM, 
-    #                                 sourceDataFolderName=pchomeFoler, latestBoolean=1)
-    # end = timeCalculate()
-    # print(f"最新檔案 {pchomeFoler}, {momoFolder} 花費：", end-begin, "秒。")
+    writeEcommerceHistoricalData(multiSourceObject, 
+                        sourceDataFolderName_1=pchomeFoler, sourceDataFolderName_2=momoFolder, alterStillWork=1)
 
 
