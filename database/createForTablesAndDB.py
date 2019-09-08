cf6a2cccc0ee58ca0f66027e3d8e1310abc3056b
UnCarter25le <UnCarter25le@gmail.com>
2019-09-08 16:50:03 +0800

    improve the main() and the way to call FK constrai

--- database/createForTablesAndDB.py
+++ database/createForTablesAndDB.py
@@ -36,6 +36,63 @@ from libs.sqlDMLAndsqlAlchemyORM import writeDataWhenMunging
 
 
 
+def dropTables(engine, conn, sqlDDL=None):
+    
+    if not sqlDDL is None:
+        """
+        使用raw sqlstring drop tables
+        """
+        for table in sqlDDL.getTables():
+            conn.execute(f"DROP TABLE if exists {table}")
+    else:
+        """
+        使用ORM 清空所有表格資料  <class 'sqlalchemy.sql.schema.Table'>
+        綁定引擎的MetaData，等於是資料庫的元資料，可以用來操控資料表class。
+
+        --->MetaData(bind=Engine(mysql+pymysql://root:***@localhost:3306/iSelect3C?charset=utf8mb4))
+        """
+        MetaData = sqla.MetaData(bind=engine, reflect=True) 
+        trans = conn.begin() 
+        for table in MetaData.sorted_tables:
+            print(table)
+            conn.execute(table.delete())
+        trans.commit()
+        trans.close()
+
+
+def aa(tableClassBase):
+    """
+    叫出所有表格名稱（字串）# 因為所有的class都繼承 Base，因此用Base呼叫metadata元資料方法，可以叫出表格的名稱。
+    """
+    for t in tableClassBase._Base.metadata.tables:  # 
+        if t == "weather_records_by_months":
+            print(1)
+        print(type(t))
+
+
+
+def CreateTables(sqlDDL, conn, tableClassBase=None, MySQL=None, AzureMSSQL=None):
+    """
+    利用 ORM 建立所有class資料表！----------------------若只想單獨重建表格「news_title_from_selenium」，則需要將相關聯的表格一起drop，才能重建。不想要同時重建的表格，必須註解。
+    
+    """
+
+    if AzureMSSQL and tableClassBase:
+        tableClassBase._BaseAzure.metadata.create_all(conn)
+        for alterSyn in sqlDDL.getMySQLAlterSyntax():
+            conn.execute(alterSyn)
+    elif MySQL and tableClassBase: #Mysql
+        tableClassBase._Base.metadata.create_all(conn)
+        for alterSyn in sqlDDL.getMySQLAlterSyntax():
+            conn.execute(alterSyn)
+    else: #raw SQLString
+        """
+        使用raw sqlString創建----------------------
+        """
+        for syn in sqlDDL.getMySQLSyntax():
+            conn.execute(syn)
+
+
 
 if __name__ == '__main__':
     # must create a database in advance:
@@ -57,56 +114,21 @@ if __name__ == '__main__':
     conn = engine.connect()
     trans = conn.begin()
 
-    foreignKeyClose, foreignKeyOpen = sqlObjectInitial.loadCorrectForeignKeyConstraintSet(tableClassBase.databaseName)
-    
-    print(foreignKeyClose, foreignKeyOpen)
+    foreignKeyClose, foreignKeyOpen = sqlObjectInitial().loadCorrectForeignKeyConstraintSet(tableClassBase.databaseName)
+
     try:
-        conn.execute(f"{foreignKeyClose}")
-
         
-
-        #------------------------------------------------------------------------------------------
-
-        # 使用raw sqlstring drop tables
-        # for table in sqlDDL.getTables():
-        #     conn.execute(f"DROP TABLE if exists {table}")
-
-
-        # 使用ORM 清空所有表格資料  <class 'sqlalchemy.sql.schema.Table'>
-        # 綁定引擎的MetaData，等於是資料庫的元資料，可以用來操控資料表class。
-        # MetaData = sqla.MetaData(bind=engine, reflect=True) 
-        # --->MetaData(bind=Engine(mysql+pymysql://root:***@localhost:3306/iSelect3C?charset=utf8mb4))
-        # trans = conn.begin() 
-        # for table in MetaData.sorted_tables:
-            # print(table)
-        #     conn.execute(table.delete())
-        # trans.commit()
+        # sqlObjectInitial.modifyForeignKeyConstraint(conn, foreignKeyClose=foreignKeyClose)
         
-        #------------------------------------------------------------------------------------------
-
-        # 叫出所有表格名稱（字串）# 因為所有的class都繼承 Base，因此用Base呼叫metadata元資料方法，可以叫出表格的名稱。
-        # for t in tableClassBase._Base.metadata.tables:  # 
-        #     if t == "weather_records_by_months":
-        #         print(1)
-        #     print(type(t))
-
-        #------------------------------------------------------------------------------------------
-
-
-        # 使用raw sqlString創建----------------------
-        # for syn in sqlDDL.getMySQLSyntax():
-        #     conn.execute(syn)
-
-        # # 利用 ORM 建立所有class資料表！----------------------若只想單獨重建表格「news_title_from_selenium」，則需要將相關聯的表格一起drop，才能重建。不想要同時重建的表格，必須註解。
-        # tableClassBase._Base.metadata.create_all(conn)
-        # for key in iter(rawSQLString().MySQLAlterUniqueRawStringDict):
-        #     alterSQLString = rawSQLString().MySQLAlterUniqueRawStringDict[key]
-        #     if alterSQLString:
-        #         conn.execute(alterSQLString)
-
+        #--------------------------drop tables----------------------------------------------------------------
         
+        # dropTables(engine, conn, sqlDDL=sqlDDL)
         
+        #-------------------------establish tables-----------------------------------------------------------------
+
+        # CreateTables(sqlDDL, conn, tableClassBase=tableClassBase, AzureMSSQL=1)
         
+        print()
     except (sqla.exc.InternalError, sqla.exc.ProgrammingError, sqla.exc.IntegrityError) as e:
         trans.rollback()
         # conn.rollback()  # AttribueError
@@ -116,7 +138,7 @@ if __name__ == '__main__':
     else:
         print("establish table:\n", "\n".join([t for t in tableClassBase.getTables()]))
     finally:
-        conn.execute(f"{foreignKeyOpen}")
+        # sqlObjectInitial.modifyForeignKeyConstraint(conn, foreignKeyOpen=foreignKeyOpen)
         trans.close()
         conn.close()
     
