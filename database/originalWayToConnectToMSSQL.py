07a99a479d7e275d26c5d1d89e39a4989c4f29ce
UnCarter25le <UnCarter25le@gmail.com>
2020-02-20 12:36:59 +0800

    add more description

--- database/originalWayToConnectToMSSQL.py
+++ database/originalWayToConnectToMSSQL.py
@@ -5,6 +5,17 @@ import pymssql
 conn = pymssql.connect(server="172.17.0.4", user="SA",
                   password="---", database="iSelect3C")
 
+cursor = conn.cursor()
+
+cursor.execute("SELECT top(1)* FROM observation_stations")
+
+row = cursor.fetchone()  
+
+
+print(row)
+
+cursor.close()
+conn.close()
 
 
 
